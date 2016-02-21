aec926d2388860b81aa89c170783cc407b10eaa1
dayatz <dayat.py@gmail.com>
2016-02-21 20:55:48 +0800

    add send_message to user viewset

--- gcm_chat/app/core/views.py
+++ gcm_chat/app/core/views.py
@@ -1,8 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import detail_route
 from .models import User
 from .serializers import UserSerializer
+from .functions import send_user_message
 
 
 class LoginRequiredMixin(object):
@@ -13,3 +15,8 @@ class LoginRequiredMixin(object):
 class UserViewSet(LoginRequiredMixin, ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @detail_route(methods=['POST'])
+    def send_message(self, request, pk):
+        message = request.data.get('message')
+        send_user_message(pk, message)
