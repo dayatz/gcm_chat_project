d97184644f8a8dee96bb846d317fc9238947a7bf
dayatz <dayat.py@gmail.com>
2016-02-22 16:37:20 +0800

    create function for send message to multi user

--- gcm_chat/app/core/functions.py
+++ gcm_chat/app/core/functions.py
@@ -1,6 +1,13 @@
 from gcm.models import Device
+from gcm.api import GCMMessage
 
 
 def send_user_message(user_id, message):
     device = Device.objects.get(dev_id=user_id)
     device.send_message({'message': message})
+
+
+def send_multiuser_message(user_ids, message):
+    reg_ids = Device.objects.filter(
+        dev_id__in=user_ids).values_list('reg_id', flat=True)
+    GCMMessage.send({'message': message}, registration_ids=reg_ids)
